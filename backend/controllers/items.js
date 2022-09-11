7c46d3a34b4589cea81b573fe7a91a063e64f3ec
MustafaMHamdan <hamdan.mustafamof@gmail.com>
2022-09-11 14:35:45 +0300

    delete item  by it,s id

--- backend/controllers/items.js
+++ backend/controllers/items.js
@@ -75,8 +75,59 @@ const getItemById = (req, res) => {
   });
 };
 
+//this function to delete an item
+
+const deleteItemById = (req, res) => {
+  const id = req.params.id;
+  const owner_id = req.token.userId;
+
+  const query = `SELECT * FROM items WHERE id=? AND is_deleted=0  ;`;
+
+  const data = [id ,owner_id];
+
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "Server Error",
+        err: err.message,
+      });
+    }
+    if (!result.length) {
+      return res.status(404).json({
+        success: false,
+        massage: "The item is Not Found",
+      });
+    }
+ if (result[0].owner_id!=owner_id) { 
+   
+    
+    return res.status(201).json({
+    
+    success: false,
+    massage: `you are not authorized to delete this item`,
+  });
+    
+ }
+
+    const query2 = `UPDATE items SET is_deleted=1 WHERE   owner_id=?;`;
+    const data2 = [owner_id];
+
+    connection.query(query2, data2, (err, result2) => {
+        
+      
+      res.status(200).json({
+        success: true,
+        massage: `Succeeded to delete item with id: ${id}`,
+        result: result,
+      });
+    });
+  });
+};
+
 module.exports = {
   setNewItem,
   getAllItems,
   getItemById,
+  deleteItemById,
 };
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { setNewItem ,getAllItems,getItemById} = require("../controllers/items");
+const { setNewItem ,getAllItems,getItemById ,deleteItemById} = require("../controllers/items");
 
 const authentication = require("../middleware/authentication");
 
@@ -9,5 +9,6 @@ const itemRouter = express.Router();
 itemRouter.post("/",authentication, setNewItem);
 itemRouter.get("/", getAllItems);
 itemRouter.get("/:id", getItemById);
+itemRouter.put("/:id",authentication, deleteItemById);
 
 module.exports = itemRouter;
